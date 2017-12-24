766f06cfac8a04772affd977a88d61210064e598
Shane Logsdon <shane@shanelogsdon.com>
2017-12-24 18:10:19 -0500

    bump version to 0.1.0

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Sap.Mixfile do
 
   def project do
     [app: :sap,
-     version: "0.0.3",
+     version: "0.1.0",
      elixir: "~> 1.0",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
